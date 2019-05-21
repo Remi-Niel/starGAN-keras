447f42ef4f2030d164a5d0f151fdfbf831aa15cf
Borg <mico@borg.com>
2019-05-21 16:50:33 +0200

    added gradient penalty

--- solver_test_broken.py
+++ solver_test_broken.py
@@ -1,9 +1,10 @@
 from model import get_generator, get_discriminator
 import tensorflow as tf
 import keras
-from keras.engine.topology import Layer
+from keras import backend as K
 from keras.models import Model
 from keras.layers import Input
+from keras.layers.merge import _Merge
 import numpy as np
 import os
 import time
@@ -12,25 +13,53 @@ import datetime
 import matplotlib.pyplot as plt
 from io import BytesIO
 from tqdm import trange
-from keras.layers.merge import _Merge
-import keras.backend as K
+from functools import partial
 
-class GradNorm(Layer):
-    def __init__(self, **kwargs):
-        super(GradNorm, self).__init__(**kwargs)
+def gradient_penalty_loss(y_true, y_pred, averaged_samples):
+    """Calculates the gradient penalty loss for a batch of "averaged" samples.
+    In Improved WGANs, the 1-Lipschitz constraint is enforced by adding a term to the
+    loss function that penalizes the network if the gradient norm moves away from 1.
+    However, it is impossible to evaluate this function at all points in the input
+    space. The compromise used in the paper is to choose random points on the lines
+    between real and generated samples, and check the gradients at these points. Note
+    that it is the gradient w.r.t. the input averaged samples, not the weights of the
+    discriminator, that we're penalizing!
+    In order to evaluate the gradients, we must first run samples through the generator
+    and evaluate the loss. Then we get the gradients of the discriminator w.r.t. the
+    input averaged samples. The l2 norm and penalty can then be calculated for this
+    gradient.
+    Note that this loss function requires the original averaged samples as input, but
+    Keras only supports passing y_true and y_pred to loss functions. To get around this,
+    we make a partial() of the function with the averaged_samples argument, and use that
+    for model training."""
+    # first get the gradients:
+    #   assuming: - that y_pred has dimensions (batch_size, 1)
+    #             - averaged_samples has dimensions (batch_size, nbr_features)
+    # gradients afterwards has dimension (batch_size, nbr_features), basically
+    # a list of nbr_features-dimensional gradient vectors
+    gradients = K.gradients(y_pred, averaged_samples)
+    # compute the euclidean norm by squaring ...
+    gradients_sqr = K.square(gradients)
+    #   ... summing over the rows ...
+    gradients_sqr_sum = K.sum(gradients_sqr,
+                              axis=np.arange(1, len(gradients_sqr.shape)))
+    #   ... and sqrt
+    gradient_l2_norm = K.sqrt(gradients_sqr_sum)
+    # compute lambda * (1 - ||grad||)^2 still for each single sample
+    gradient_penalty = K.square(1 - gradient_l2_norm)
+    # return the mean as loss over all the batch samples
+    return K.mean(gradient_penalty)
 
-    def build(self, input_shapes):
-        super(GradNorm, self).build(input_shapes)
 
-    def call(self, inputs):
-        target, wrt = inputs
-        grads = K.gradients(target, wrt)
-        assert len(grads) == 1
-        grad = grads[0]
-        return K.sqrt(K.sum(K.batch_flatten(K.square(grad)), axis=1, keepdims=True))
+class RandomWeightedAverage(_Merge):
+    """Takes a randomly-weighted average of two tensors. In geometric terms, this
+    outputs a random point on the line between each pair of input points.
+    Inheriting from _Merge is a little messy but it was the quickest solution I could
+    think of. Improvements appreciated."""
 
-    def compute_output_shape(self, input_shapes):
-        return (input_shapes[1][0], 1)
+    def _merge_function(self, inputs):
+        weights = K.random_uniform((self.BATCH_SIZE, 1, 1, 1))
+        return (weights * inputs[0]) + ((1 - weights) * inputs[1])
 
 class Solver(object):
 
@@ -88,47 +117,47 @@ class Solver(object):
         less than 0."""
         return keras.backend.mean(y_true * y_pred)
 
-    def multiple_loss(y_true, y_pred):
-        return K.mean(y_true*y_pred)
 
-    def mean_loss(y_true, y_pred):
-        return K.mean(y_pred)
-
-    class Subtract(_Merge):
-        def _merge_function(self, inputs):
-            output = inputs[0]
-            for i in range(1, len(inputs)):
-                output = output-inputs[i]
-            return output
-
-    #http://shaofanlai.com/post/10
     def build_model(self):
         self.G = get_generator(self.g_conv_dim, self.n_labels, self.g_repeat_num, self.image_size)
-        self.D = get_discriminator(self.d_conv_dim, self.n_labels, self.d_repeat_num, self.image_size)
+        input_img, out_src, out_cls = get_discriminator(self.d_conv_dim, self.n_labels, self.d_repeat_num, self.image_size)
+
+        self.D_a = Model(inputs = [input_img], outputs = [out_src, out_cls])
+
+        concatted_input = Input(shape=(self.image_size,self.image_size,3+self.n_labels))
+        gen_samples = self.G(concatted_input)
+
+        weighted_avg = RandomWeightedAverage()
+        weighted_avg.BATCH_SIZE=self.batch_size
+        averaged_samples = weighted_avg([input_img, gen_samples])
+        averaged_samples_out = self.D_a(averaged_samples)[0]
+
+        print(averaged_samples_out)
+        print(averaged_samples_out.shape)
+
+        partial_gp_loss = partial(gradient_penalty_loss, averaged_samples = averaged_samples)
+        partial_gp_loss.__name__ = 'gradient_penalty'
+
 
         self.d_optimizer = keras.optimizers.Adam(lr = self.d_lr, beta_1 = self.beta_1, beta_2 = self.beta_2, decay = 1.0/self.num_iters_decay/5)
         self.g_optimizer = keras.optimizers.Adam(lr = self.g_lr, beta_1 = self.beta_2, beta_2 = self.beta_2, decay = 1.0/self.num_iters_decay/5)
+
+
+        self.D = Model(inputs =[input_img, concatted_input], outputs=[out_src, out_cls, averaged_samples_out])
+
+        self.D.compile(loss=[self.wasserstein_loss, "binary_crossentropy", partial_gp_loss], loss_weights = [1, self.lambda_cls, self.lambda_gp], optimizer= self.d_optimizer)
 
         self.D.trainable = False
 
         input_img = Input(shape = (self.image_size, self.image_size, 3 + self.n_labels))
 
         reconstr_img = self.G(input_img)
-        output_D     = self.D(reconstr_img)
+        output_D     = self.D_a(reconstr_img)
+
 
         self.combined = Model(inputs = [input_img], outputs = [reconstr_img] + output_D)
 
         self.combined.compile(loss = ["mae", self.wasserstein_loss, "binary_crossentropy"], loss_weights = [self.lambda_rec, -1, self.lambda_cls], optimizer = self.g_optimizer)
-
-        shape = (self.image_size,self.image_size,3)
-        gen_input, interpolation = Input(shape), Input(shape)
-        norm = GradNorm()([self.D(interpolation)[0], interpolation])
-        output_D = self.D(gen_input)
-        self.dis2batch = Model([gen_input, interpolation], output_D + [norm])
-
-        self.D.trainable = True
-
-        self.dis2batch.compile(loss=[self.wasserstein_loss, "binary_crossentropy", 'mse'], loss_weights = [1, self.lambda_cls, self.lambda_gp], optimizer= self.d_optimizer)
 
     def label2onehot(self, labels, dim):
         """Convert label indices to one-hot vectors."""
@@ -180,6 +209,8 @@ class Solver(object):
         label_test = np.tile(label_test, (5,1))
         test_imgs_concatted = np.concatenate((test_imgs, np.tile(labels_fixed, (1,self.image_size, self.image_size,1))), axis=3)
 
+        dummy_y = np.zeros((self.batch_size * 2, 4), dtype=np.float32)
+
         for epoch in trange(0,self.num_iters//self.log_step//5):
             with keras.backend.get_session().as_default():
 
@@ -225,6 +256,7 @@ class Solver(object):
 
 
                     x_fake = self.G.predict(x_concatted)
+                    x_concatted_fake = np.concatenate((x_fake, np.tile(labels_trg, (1,self.image_size, self.image_size,1))), axis=3)
 
 
                     fake = np.zeros(self.batch_size)
@@ -232,21 +264,9 @@ class Solver(object):
                     concatted_bool = np.concatenate((fake,real))
                     concatted_labels = np.concatenate((c_trg,c_org))
                     concatted_imgs = np.concatenate((x_fake, x_real))
+                    concatted_gen_input = np.concatenate((x_concatted_fake,x_concatted))
 
-                    concatted_labels_inv = np.flip(concatted_labels, axis = 0)
-
-                    concatted_fake_imgs = self.G.predict(np.concatenate((concatted_imgs, 
-                                                                            np.tile(concatted_labels_inv.reshape(self.batch_size*2,1,1,5), 
-                                                                                        (1,self.image_size, self.image_size,1))
-                                                                                    ), 
-                                                                        axis = 3
-                                                                        )
-                                                        )
-
-                    epsilon = np.random.uniform(0, 1, size = (2 * self.batch_size,1,1,1))
-                    interpolation = epsilon * concatted_imgs + (1-epsilon) * concatted_fake_imgs
-
-                    d_loss_r = self.dis2batch.train_on_batch([concatted_imgs, interpolation], [np.tile(concatted_bool.reshape(self.batch_size*2,1),(1,4)), concatted_labels, np.ones(self.batch_size * 2)])
+                    d_loss_r = self.D.train_on_batch([concatted_imgs, concatted_gen_input], [np.tile(concatted_bool.reshape(self.batch_size*2,1),(1,4)), concatted_labels, dummy_y])
 
                 g_loss = self.combined.train_on_batch(x_concatted, [x_real, np.tile(fake.reshape(self.batch_size,1),(1,4)), c_trg])
 
