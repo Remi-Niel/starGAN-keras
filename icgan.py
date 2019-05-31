b04a39217533da9f92606ed6a8817d2bac7b340b
Borg <dfskljhasf@asdkljhflas.nl>
2019-05-31 16:32:05 +0200

    working cgan

--- icgan.py
+++ icgan.py
@@ -7,14 +7,14 @@ def get_generator(n_filters = 64, n_labels = 5, repeat_num = 6, im_size = 100):
     input_img = tf.keras.layers.Input(shape = (1,1,im_size))
     input_labels = tf.keras.layers.Input(shape=[1,1,n_labels])
     x = tf.keras.layers.Concatenate()([input_img,input_labels])
-    x = tf.keras.layers.Reshape((1,1,100+n_labels))(x)
+    x = tf.keras.layers.Reshape((1,1,im_size+n_labels))(x)
 
     x = tf.keras.layers.Conv2DTranspose(filters=512, kernel_size=4,
-                               strides=4, padding='SAME')(x)
+                               strides=2, padding='VALID')(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.ReLU()(x)
 
-    depth = [256, 128, 64]
+    depth = [256, 256, 128, 64]
     for idx, filters in enumerate(depth):
         x = tf.keras.layers.Conv2DTranspose(filters=filters, kernel_size=4,
                                    strides=2, padding='SAME')(x)
@@ -27,35 +27,37 @@ def get_generator(n_filters = 64, n_labels = 5, repeat_num = 6, im_size = 100):
 
     generator = tf.keras.Model([input_img, input_labels], x, name = 'generator')
 
-    print(generator.summary())
+    # print(generator.summary())
     return generator
 
 # Create and return discriminator model
-def get_discriminator(n_filters = 64, n_labels = 5, repeat_num = 6, im_size = 64):
+def get_discriminator(n_filters = 64, n_labels = 5, repeat_num = 6, im_size = 128):
     input_img = tf.keras.layers.Input(shape = (im_size, im_size, 3))
     input_labels = tf.keras.layers.Input(shape=[n_labels])
     labels = tf.keras.layers.Reshape((1,1,n_labels))(input_labels)
-    labels = tf.keras.layers.Lambda(tf.keras.backend.tile, arguments = {'n': (1,32,32,1)})(labels)
+    labels = tf.keras.layers.Lambda(tf.keras.backend.tile, arguments = {'n': (1,64,64,1)})(labels)
 
-    x = tf.keras.layers.Conv2D(filters=64, kernel_size=4,
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=4,
                                strides=2, padding='SAME')(input_img)    
     x = tf.keras.layers.LeakyReLU()(x)
     x = tf.keras.layers.Concatenate()([x,labels])
 
-    depth = [128,256,512]
+    print(x.shape)
+    depth = [128, 256, 256, 512]
     for idx, filters in enumerate(depth):
         x = tf.keras.layers.Conv2D(filters=filters, kernel_size=4,
                                    strides=2, padding='SAME')(x)
         x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.LeakyReLU()(x)        
+        x = tf.keras.layers.LeakyReLU()(x)       
+        print(x.shape) 
 
     x = tf.keras.layers.Conv2D(filters=1, kernel_size=4,
-                               strides=1, padding='SAME', activation='sigmoid')(x)
+                               strides=1, padding='VALID', activation='sigmoid')(x)
     # x = tf.keras.layers.Activation('sigmoid')(x)
     # x = tf.keras.activations.sigmoid(x)
 
     discriminator = tf.keras.Model([input_img, input_labels], x, name = 'discriminator')
-
+    print(discriminator.summary())
     return discriminator
 
 def get_encoder_ez(n_labels = 5, im_size = 128):
